fa729a0072c6c4b9a8ed4cff133b8103dcf45e14
CharlieYu4994 <charlieyu4994@outlook.com>
2022-08-24 00:23:40 +0800

    rename the display driver folder

--- main/main.c
+++ main/main.c
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2022 CharlieYu4994
- * 
+ *
  * This software is released under the MIT License.
  * https://opensource.org/licenses/MIT
  */
@@ -20,25 +20,28 @@
 #include "lvgl.h"
 #include "ssd1316.h"
 
+#define TICK_PERIOD 1
+
 static void lv_tick_task(void *arg);
 static void guiTask(void *pvParameter);
 SemaphoreHandle_t xGuiSemaphore;
 
 void app_main(void)
 {
-    xTaskCreate(guiTask, "gui", 4096*2, NULL, 0, NULL);
+    xTaskCreate(guiTask, "gui", 4096 * 2, NULL, 0, NULL);
 }
 
-static void guiTask(void *pvParameter) {
+static void guiTask(void *pvParameter)
+{
 
-    (void) pvParameter;
+    (void)pvParameter;
     xGuiSemaphore = xSemaphoreCreateMutex();
 
     lv_init();
 
     ssd1316_init();
 
-    lv_color_t* buf1 = heap_caps_malloc(2048 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    lv_color_t *buf1 = heap_caps_malloc(2048 * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf1 != NULL);
     static lv_color_t *buf2 = NULL;
     static lv_disp_draw_buf_t disp_buf;
@@ -57,27 +60,29 @@ static void guiTask(void *pvParameter) {
 
     const esp_timer_create_args_t periodic_timer_args = {
         .callback = &lv_tick_task,
-        .name = "periodic_gui"
-    };
+        .name = "periodic_gui"};
     esp_timer_handle_t periodic_timer;
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, TICK_PERIOD * 1000));
 
-    while (1) {
+    while (1)
+    {
         vTaskDelay(pdMS_TO_TICKS(10));
 
-        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY))
+        {
             lv_task_handler();
             xSemaphoreGive(xGuiSemaphore);
-       }
+        }
     }
 
     free(buf1);
     vTaskDelete(NULL);
 }
 
-static void lv_tick_task(void *arg) {
-    (void) arg;
+static void lv_tick_task(void *arg)
+{
+    (void)arg;
 
     lv_tick_inc(1);
 }
